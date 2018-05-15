95237e3eb774616b24f3a213f0d21f2bf423932e
Manuel Rafeli <rafelim@ifmd.it>
2018-05-15 11:46:54 +0200

    new commit

--- src/main/java/com/example/jwt/examplejwt/service/impl/GenericServiceImpl.java
+++ src/main/java/com/example/jwt/examplejwt/service/impl/GenericServiceImpl.java
@@ -3,9 +3,7 @@ package com.example.jwt.examplejwt.service.impl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.example.jwt.examplejwt.domain.RandomCity;
 import com.example.jwt.examplejwt.domain.User;
-import com.example.jwt.examplejwt.repository.RandomCityRepository;
 import com.example.jwt.examplejwt.repository.UserRepository;
 import com.example.jwt.examplejwt.service.GenericService;
 
@@ -19,9 +17,6 @@ public class GenericServiceImpl implements GenericService {
     @Autowired
     private UserRepository userRepository;
 
-//    @Autowired
-//    private RandomCityRepository randomCityRepository;
-
     @Override
     public User findByUsername(String username) {
         return userRepository.findByUsername(username);
@@ -31,9 +26,4 @@ public class GenericServiceImpl implements GenericService {
     public List<User> findAllUsers() {
         return (List<User>)userRepository.findAll();
     }
-
-//    @Override
-//    public List<RandomCity> findAllRandomCities() {
-//        return (List<RandomCity>)randomCityRepository.findAll();
-//    }
 }
